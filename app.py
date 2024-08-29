1b11e2384c62639e7915c17b1e232f7569d0e36c
emonyao <57286016+emonyao@users.noreply.github.com>
2024-08-29 19:40:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/',methods=["get","post"])
+def store_money():
+    return render_template('store_money.html')
+
+@app.route('/',methods=["get","post"])
+def transfer_money():
+    return render_template('transfer_money.html')
+
 if __name__=='__main__':
     app.run()
